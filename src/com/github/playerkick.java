cd724e3f6fc54dbeeb8909843f1c1859ec8dad70
pegberink <p.egberink@g0lden23.nl>
2013-11-23 16:27:19 +0100

    Working update
    Changed the login to joinEvent

--- src/com/github/playerkick.java
+++ src/com/github/playerkick.java
@@ -3,11 +3,11 @@ package com.github;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class playerkick implements Listener {
 		@EventHandler
-	    public void onPlayerLoginEvent(PlayerLoginEvent event){
+	    public void onPlayerJoinEvent(PlayerJoinEvent event){
 		Player p = event.getPlayer();
 		p.kickPlayer("Code = ");
 		}
