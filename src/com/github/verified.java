25768b80f5ae094400d19f8f7eaff215392784fd
pegberink <p.egberink@g0lden23.nl>
2013-11-23 16:06:16 +0100

    update#003

--- src/com/github/verified.java
+++ src/com/github/verified.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class verified extends JavaPlugin {
 	public void onEnable(){
 		getLogger().info("Verification Plugin enabled!");
+		getServer().getPluginManager().registerEvents(new playerkick(), this);
 	}
 	public void onDisable(){
 		getLogger().info("Verification plugin disabled");
